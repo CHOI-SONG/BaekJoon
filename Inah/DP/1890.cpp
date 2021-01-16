2a506dbb0ef78d6b710337e1da155467b77c984c
jody9852 <61678447+jody9852@users.noreply.github.com>
2021-01-17 00:12:49 +0900

    Create 1890.cpp

---
+++ Inah/DP/1890.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, map[101][101];
+	long dp[101][101] = { 0 };
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	dp[0][0] = 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (dp[i][j] == 0 || (i == n - 1 && j == n - 1)) continue;
+
+			int dist = map[i][j];
+			int down = dist + i; 
+			int right = dist + j;
+
+			if (down < n) dp[down][j] += dp[i][j];
+			if (right < n) dp[i][right] += dp[i][j];
+		}
+	}
+
+	cout << dp[n - 1][n - 1];
+	return 0;
+}
